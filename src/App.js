8bf932a4aebe50f30614bade0b1e1cecb1373f6f
sakhrudin <magnat0610@gmail.com>
2023-01-19 23:45:00 +0800

    добавлена страница notfound

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Header from './components/Header';
 import './scss/app.scss';
 import Home from './pages/Home';
 import NotFoundBlock from './components/notFoundBlock';
+import { Route, Routes } from 'react-router-dom';
+import Cart from './pages/Cart';
 
 function App() {
  
@@ -11,8 +13,11 @@ function App() {
     <div className="wrapper">
       <Header />
       <div className="content">
-       {/* <Home/> */}
-       <NotFoundBlock />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='*' element={<NotFoundBlock />} />
+        </Routes>
       </div>
     </div>
   );
---
+++ src/pages/Cart.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cart = () => {
+  return (
+    <div>
+      корзина
+    </div>
+  );
+};
+
+export default Cart;
