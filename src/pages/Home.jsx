32357439a5a4d79a95ddbf640e54b76dc0853bb2
sakhrudin <magnat0610@gmail.com>
2023-01-23 22:28:41 +0800

    added cart

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,34 @@
 import React from "react";
-import Categiries from '../components/Categories';
-import PizzaBlock from '../components/PizzaBlock';
-import { Skeleton } from '../components/PizzaBlock/Skeleton';
-import Sort from '../components/Sort';
+import Categiries from "../components/Categories";
+import PizzaBlock from "../components/PizzaBlock";
+import { Skeleton } from "../components/PizzaBlock/Skeleton";
+import Sort from "../components/Sort";
 
 const Home = () => {
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
   React.useEffect(() => {
-    fetch('https://63bad84332d17a50907f241b.mockapi.io/items')
+    fetch("https://63bad84332d17a50907f241b.mockapi.io/items")
       .then((res) => res.json())
       .then((arr) => {
         setItems(arr);
-        setIsLoading(false)
-      })
-  }, [])
+        setIsLoading(false);
+      });
+    window.scrollTo(0, 0);
+  }, []);
   return (
-    <>
-      <div className="container">
-        <div className="content__top">
-          <Categiries />
-          <Sort />
-        </div>
-        <h2 className="content__title">Все пиццы</h2>
-        <div className="content__items">
-          {isLoading
-            ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-            : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
-        </div>
+    <div className="container">
+      <div className="content__top">
+        <Categiries />
+        <Sort />
       </div>
-    </>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+      </div>
+    </div>
   );
 };
 
