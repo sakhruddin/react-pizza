0612a36f839c28409c08c57a8f127def4c745486
sakhrudin <magnat0610@gmail.com>
2023-01-31 22:49:54 +0800

    закончена сортировка

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,17 +10,19 @@ const Home = () => {
   const [categoryId, setCategoryId] = React.useState(0);
   const [sortType, setSortType] = React.useState({
     name: "Популярности",
-    sortProperty: "rating"
+    sortProperty: "rating",
   });
 
   React.useEffect(() => {
     setIsLoading(true);
 
-    const sortBy = sortType.sortProperty.replace('-', '');
-    const order = sortType.sortProperty.includes('-')  ? 'asc' : 'desc';
-    const category = categoryId > 0 ? `category=${categoryId}` : '';
+    const sortBy = sortType.sortProperty.replace("-", "");
+    const order = sortType.sortProperty.includes("-") ? "asc" : "desc";
+    const category = categoryId > 0 ? `category=${categoryId}` : "";
 
-    fetch(`https://63bad84332d17a50907f241b.mockapi.io/items?${category}&sortBby=${sortBy}&order=${order}`,)
+    fetch(
+      `https://63bad84332d17a50907f241b.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`
+    )
       .then((res) => res.json())
       .then((arr) => {
         setItems(arr);
@@ -28,11 +30,15 @@ const Home = () => {
       });
     window.scrollTo(0, 0);
   }, [categoryId, sortType]);
+
   return (
     <div className="container">
       <div className="content__top">
-        <Categiries value={categoryId} onChangeCategory={(i)=> setCategoryId(i)}/>
-        <Sort value={sortType} onChangeSort={(i)=> setSortType(i)}/>
+        <Categiries
+          value={categoryId}
+          onChangeCategory={(i) => setCategoryId(i)}
+        />
+        <Sort value={sortType} onChangeSort={(i) => setSortType(i)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
