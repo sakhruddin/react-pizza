6e5a6fccb863916861a11343d5d0840730378d5f
sakhrudin <magnat0610@gmail.com>
2023-03-08 13:02:18 +0800

    завершена сортировка на redux

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -4,7 +4,9 @@ import { MdOutlineClear } from "react-icons/md";
 import { SearchContext } from "../../App";
 
 const Search = () => {
- const { searchValue, setSearchValue } = React.useContext(SearchContext)
+
+ const { searchValue, setSearchValue } = React.useContext(SearchContext);
+ 
   return (
     <div className={styles.root}>
       <svg
